e2a0d4a048f50b2c36f971fec76b21aab05a5d23
abachschmid <>
2022-01-18 02:59:23 +0100

    quickfix

--- production/Funktionen.js
+++ production/Funktionen.js
@@ -1,7 +1,8 @@
 // just
 // justConnect
-
 var coha = {}
+
+coha.log = console.log
 
 coha.getActiveChapterTitle = () => {
   // Get 
@@ -11,20 +12,64 @@ coha.getActiveChapterTitle = () => {
   return (activeChapter ?? activeWiki)?.split("/")?.pop()
 }
 
-coha.asyncCall = async article => {
+coha.httpConvertHtmlToProseMirror = async article => {
+  coha.log('HTTP HTML TO PROSE', article)
   try {
-    const res = await jQuery.ajax({
-      url: 'https://www.google.de/'+article.title,
+    return await jQuery.ajax({
+      url: 'https://prosemirror-converter.tools.corporate-happiness.de',
       type: 'GET',
+      data: {
+        data: article.html
+      }
     });
-    console.log(res, article)
   } catch(err) {
-    console.log(err);
+    return {}
+  }
+
+}
+
+coha.httpCreateArticle = async article => {  
+  coha.log('HTTP CREATE ARTICLE', article)
+  try {
+    const isArticle = article.type === 'article'
+    return await jQuery.ajax({
+      url: '/wiki/graphql',
+      type: 'POST',
+      headers: {
+        'just-tenant-id': just?.context?.config?.tenantId,
+        'just-token': just?.auth?.token,
+      },
+      contentType: "application/json",
+      // data: article.data
+      data: JSON.stringify({
+        operationName: isArticle ? 'createArticle': 'createChapter',
+        query: isArticle 
+          ? "mutation createArticle($title: String!, $content: String!, $parentId: ParentId!, $textContent: String!, $contactPerson: ProfileId) {\n  createdArticle: createArticle(\n    article: {title: $title, content: $content, textContent: $textContent, parentId: $parentId, contactPerson: $contactPerson}\n  ) {\n    id\n    title\n    content\n    __typename\n  }\n}\n"
+          : "mutation createChapter($title: String!, $parentId: ParentId!) {\n  createdChapter: createChapter(chapter: {title: $title, parentId: $parentId}) {\n    id\n    title\n    __typename\n  }\n}\n",
+
+        variables: {
+          title: article.title,
+          parentId: coha.getActiveChapterTitle(),
+          content: JSON.stringify(article.data),
+          // content: "{\"type\":\"doc\",\"content\":[{\"type\":\"paragraph\",\"content\":[{\"type\":\"text\",\"marks\":[{\"type\":\"strong\"}],\"text\":\"atseatse\"}]}]}",
+          textContent: "test",
+          contactPerson: "PROFILE,4",
+        },
+      }),
+    });
+  } catch(err) {
+    return {}
   }
 }
 
-if(typeof articles !== 'undefined') {
-  for(i in articles) {
-    await coha.asyncCall(articles[i])
+coha.run = async () => {
+  if(typeof articles !== 'undefined') {
+    for(i in articles) {
+      let article = articles[i]
+      article.data = await coha.httpConvertHtmlToProseMirror(article)
+      await coha.httpCreateArticle(article)
+    }
   }
 }
+
+coha.log('Coha initialized');
---
+++ test articles/Modul 1.js
@@ -0,0 +1,14 @@
+var articles = [
+	{
+    title: "a",
+    html: "Ich bin ein text"
+	},
+  {
+    title: "b",
+    html: "Ich bin ein text"
+	},
+  {
+    title: "c",
+    html: "Ich bin ein text"
+	},
+]
